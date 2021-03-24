3b0d37e43fe5ae861522b015ad295a2c1a9e65a8
tanushae <76809166+tanushae@users.noreply.github.com>
2021-03-24 16:37:20 +0530

    Add files via upload

---
+++ instructions.js
@@ -0,0 +1,8 @@
+text ("Preheat oven to 350 degrees. Butter three 9-inch cake rounds. Dust with flour and tap out the excess.")
+text ("Mix together flour, sugar, cornstarch, cocoa, baking soda, baking powder, and salt in a stand mixer using a low speed until combined.")
+text ("Add eggs, buttermilk, warm water, oil, vanilla, vinegar, and food coloring. Beat on a medium speed until smooth. This should take just a couple of minutes.")
+text ("Divide batter among the three prepared pans.")
+text ("Bake for 30-35 minutes until the cake meets the toothpick test (stick a toothpick in and it comes out clean.")
+text ("Cool on wire racks for 15 minutes and then turn out the cakes onto the racks and allow to cool completely before frosting.")
+text ("Make the frosting. In a large bowl, beat together butter and cream cheese until fluffy. Use a hand mixer or stand mixer for best results. Add in vanilla extract. Beat until combined. Beat in powdered sugar, 1 cup at a time until frosting is smooth.")
+text ("Assemble and frost the completely cooled cake.")
